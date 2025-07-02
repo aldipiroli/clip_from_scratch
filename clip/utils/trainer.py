cf72fc40cd49b127d4e88711ad045f3a147e65bc
Aldi Piroli <aldipiroli@gmail.com>
2025-07-02 23:05:28 +0200

    fixed pbar

--- clip/utils/trainer.py
+++ clip/utils/trainer.py
@@ -18,6 +18,7 @@ class Trainer(TrainerBase):
     def train(self):
         self.logger.info("Started training..")
         start_epoch = self.epoch
+        self.evaluate_model()
         for epoch in range(start_epoch, self.config["OPTIM"]["num_epochs"]):
             self.epoch = epoch
             self.train_one_epoch()
@@ -28,9 +29,7 @@ class Trainer(TrainerBase):
 
     def train_one_epoch(self):
         self.model.train()
-        self.reshuffle_dataloader()
-        train_loss = []
-        pbar = tqdm(enumerate(self.train_loader), total=self.config["OPTIM"]["num_epochs"])
+        pbar = tqdm(enumerate(self.train_loader), total=len(self.train_loader))
         for n_iter, (img, text, eos_id) in pbar:
             self.optimizer.zero_grad()
             img = img.to(self.device)
@@ -39,7 +38,6 @@ class Trainer(TrainerBase):
 
             logits_img2text, logits_text2img = self.model(img, text, eos_id)
             loss = self.loss_fn(logits_img2text, logits_text2img)
-            train_loss.append(loss)
             self.write_float_to_tb(loss, "train/loss", self.total_iters)
 
             loss.backward()
@@ -47,19 +45,28 @@ class Trainer(TrainerBase):
             self.optimizer.step()
             self.total_iters += 1
             pbar.set_postfix({"epoch": f"{self.epoch}/{self.config['OPTIM']['num_epochs']}", "loss": loss.item()})
-        self.logger.info(f"Epoch {self.epoch}/{self.num_epochs}: train loss {torch.mean(torch.tensor(train_loss))}")
+        pbar.close()
 
     @torch.no_grad()
     def evaluate_model(self):
         self.model.eval()
         eval_loss = []
-        pbar = tqdm(enumerate(self.val_loader), total=self.config["OPTIM"]["num_iterations_val"])
-        for n_iter, (img, text) in pbar:
+        pbar = tqdm(enumerate(self.val_loader), total=len(self.val_loader))
+        for n_iter, (img, text, eos_id) in pbar:
+            if n_iter > self.config["OPTIM"]["num_iterations_val"]:
+                break
+            self.optimizer.zero_grad()
             img = img.to(self.device)
             text = text.to(self.device)
-            preds = self.model(img, text)
-            loss = self.loss_fn()
+            eos_id = eos_id.to(self.device)
+
+            logits_img2text, logits_text2img = self.model(img, text, eos_id)
+            loss = self.loss_fn(logits_img2text, logits_text2img)
             eval_loss.append(loss)
+            pbar.set_postfix({"epoch": f"{self.epoch}/{self.config['OPTIM']['num_epochs']}", "eval_loss": loss.item()})
+
+        pbar.close()
+
         eval_loss = torch.tensor(eval_loss).mean()
         self.logger.info(f"Epoch {self.epoch}/{self.num_epochs}: val loss {torch.tensor(eval_loss)}")
         self.write_float_to_tb(eval_loss, "val/loss", self.epoch)
